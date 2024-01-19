02839559283672bd57d6967f1961d597c79bbf79
pramepree <pramepree159@gmail.com>
2024-01-19 15:13:20 +0700

    ครั้งที่ 2 refs-array.js
    ครั้งที่ 2 refs-array.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Button from './class-components';
 import { Header } from './func-components';
 import RefsFunc from './refs-func';
 import { Calculator } from './calculator';
-
+import RefsArray from './refs-array';
 
 function App() {
-  return <RefsFunc/>
+  return <RefsArray/>
 
 }
 
