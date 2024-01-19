a8dac69c1e44169e3936bddbb779e8afe4e500c3
pramepree <pramepree159@gmail.com>
2024-01-19 14:24:50 +0700

    ครั้งที่ 1 (วันศุกร์)
    ครั้งที่ 1 (วันศุกร์)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Button from './class-components';
+import { Header } from './func-components';
+import RefsFunc from './refs-func';
+import { Calculator } from './calculator';
+
 
 function App() {
-  return <Button/>
+  return <RefsFunc/>
+
 }
 
 export default App;
