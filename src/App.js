19b3ab0edcf4af94b04ca47efe30d4ebd66a418a
pramepree <pramepree159@gmail.com>
2024-01-19 18:07:25 +0700

    ครั้งที่ 3 state-func.js
    ครั้งที่ 3 state-func.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React, { Component } from 'react'
-import Button from './class-components';
-import { Header } from './func-components';
-import RefsFunc from './refs-func';
-import { Calculator } from './calculator';
-import RefsArray from './refs-array';
-import MessageBox from './state-func';
 
+import React, { Component } from 'react'
+import './App.css';
+import MessageBox from './state-func';
 
 function App() {
   return <MessageBox/>
