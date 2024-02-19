36e3f0b477fdc55785197273200b855e0d55bb4d
pramepree <pramepree159@gmail.com>
2024-02-19 09:55:35 +0700

    form-search.js
    form-search.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import FormSearch from "./form-search";
 import { userContext } from "./context";
 import Content2 from "./context-content";
 import Header2 from "./context-header2";
 import Router2 from "./router2";
 import Router3 from "./router3";
 export default function App() {
-  return <Router3 />;
+  return <FormSearch />;
 }
