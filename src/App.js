c62f2aaf02bc4af55d72b9627f9cee95d090e669
pramepree <pramepree159@gmail.com>
2024-03-04 22:11:43 +0700

    form-validation
    form-validation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import FormText from "./form-text";
 import FormCheck from "./form-check";
 import FormValidation from "./form-validation";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 export default function App() {
-  return  <FormValidation/>;
+  return <FormValidation />;
 }
